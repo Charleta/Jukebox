0156cfe49e3a70ec9b084a708be006af785cc2e5
Charleta <sr.carlosortega@gmail.com>
2026-04-22 23:46:21 -0300

    Disable local startup cleanup in production

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,5 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
+  if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NODE_ENV !== 'production') {
     const { prisma } = await import('./lib/db')
     await prisma.cola.deleteMany()
     console.log('[startup] Cola limpiada')
